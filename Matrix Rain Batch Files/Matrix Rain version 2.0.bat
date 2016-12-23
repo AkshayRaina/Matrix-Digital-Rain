102b4f04ffa5ee036abf7b058cf9de0fb68fe018
Akshay Raina <footballpassion25@ymail.com>
2016-12-24 01:09:59 +0530

    Add files via upload
    Batch Files -  Run Matrix Rain versions in cmd.

---
+++ Matrix Rain Batch Files/Matrix Rain version 2.0.bat
@@ -0,0 +1,30 @@
+@echo off
+:a
+color 2
+
+echo DC 06 4C 10 0E 69 03 0A 9E 21 2C 27 23 72 0F B8   ..L..i...!,'#r..
+ping localhost -n  > nul
+echo 0D 16 AA 3C 0A D1 7B 0C 0D 30 B2 C2 58 5E 6A F9   ...<..{..0..X^j.
+ping localhost -n  > nul
+echo 0B 4F 3F 41 72 BF 76 EB BB 0D 03 80 20 27 9F 91   .O?Ar.v..... '..
+ping localhost -n  > nul
+echo 91 B5 FF CF 04 23 EC 50 14 F6 50 1A 50 48 48 AA   .....#.P..P.PHH.
+ping localhost -n  > nul
+echo CC AC F5 92 7D 5D CF 82 C7 8B 51 C7 DD 17 90 71   ....}]....Q....q
+ping localhost -n  > nul
+echo 20 90 8C BD 56 37 66 AF C4 E6 FE FF FF 45 00 06    ...V7f......E..
+ping localhost -n  > nul
+echo 48 D4 A1 8A 72 84 FF CF 44 23 AC D0 0C CD 68 7A   H...r...D#....hz
+ping localhost -n  > nul
+echo 48 52 68 35 71 71 B6 2D 55 58 04 CC 3A AF 98 91   HRh5qq.-UX..:...
+ping localhost -n  > nul
+echo 25 AD 70 DF 94 62 0D 94 DD C1 06 C7 F2 35 13 40   %.p..b.......5.@
+ping localhost -n  > nul
+echo 0B 4F 3F 41 72 BF 76 EB BB 0D 03 80 20 27 9F 91   .O?Ar.v..... '..
+ping localhost -n  > nul
+echo 91 B5 FF CF 04 23 EC 50 14 F6 50 1A 50 48 48 AA   .....#.P..P.PHH.
+ping localhost -n  > nul
+echo CC AC F5 92 7D 5D CF 82 C7 8B 51 C7 DD 17 90 71   ....}]....Q....q
+ping localhost -n  > nul
+echo 20 90 8C BD 56 37 66 AF C4 E6 FE FF FF 45 00 06   ....V7f......E..
+goto a
